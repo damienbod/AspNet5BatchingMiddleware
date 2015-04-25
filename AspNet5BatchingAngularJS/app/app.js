e5f9bdc726a7d0ca3d00e6ee7d598af03cea4288
damienbod <damien_bod@hotmail.com>
2015-04-25 07:54:54 +0200

    adding angularjs batch

---
+++ AspNet5BatchingAngularJS/app/app.js
@@ -0,0 +1,27 @@
+﻿(function () {
+	'use strict';
+
+	var app = angular.module('app', [
+		'jcs.angular-http-batch'
+
+	]);
+
+	app.config(['httpBatchConfigProvider', function (httpBatchConfigProvider) {
+		httpBatchConfigProvider.setAllowedBatchEndpoint(
+				// root endpoint url
+				'http://localhost:52857',
+
+				// endpoint batch address
+				'http://localhost:52857/api/$batch',
+
+				// optional configuration parameters
+				{
+					maxBatchedRequestPerCall: 20
+				});
+	}
+	]);
+
+
+	angular.module('myApp', ['jcs.angular-http-batch']);
+
+})();
--- AspNet5BatchingAngularJS/wwwroot/_references.js
+++ AspNet5BatchingAngularJS/wwwroot/_references.js
@@ -1,2 +1,5 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gruntfile.js" />
+/// <reference path="lib/angular/angular.js" />
+/// <reference path="lib/angular-http-batcher/angular-http-batch.min.js" />
+/// <reference path="../app/app.js" />
